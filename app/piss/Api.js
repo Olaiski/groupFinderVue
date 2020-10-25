e11276299523220724753f498bb5f1cfacef4a8c
Olaiski <kungbinders@gmail.com>
2020-10-25 14:39:08 +0100

    Removed folders

--- app/piss/Api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'; // http requests til backend.
-
-export default () => { // Sender en "Connector", for å "treffe" endpoints
-    return axios.create({
-       baseURL: 'http://localhost:3000/api/',
-    });
-};
